eb770e4808230704b11a5fa1a4eb6d86de8a3171
Mohamed Ramadan <mohamedramadan6420@gmail.com>
2022-03-27 16:31:25 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:bmi/BMIScreen.dart';
+import 'package:flutter/material.dart';
+import 'bmi_result_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BmiScreen(),
+    );
+  }
+}
+
